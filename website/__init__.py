7805af2da755e0580083275c81ab708e95c72632
hussain4me <hussain4me2008@gmail.com>
2022-03-24 15:34:02 +0100

    update files

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import SQLAlchemy
+from os import path 
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -18,4 +19,16 @@ def create_app():
     app.register_blueprint(view, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    # from .models import User, Notes
+    from .models import User, Notes
+    
+    
+    create_database(app)
+    
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+    
